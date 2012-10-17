e2e50d82d8528bfdd0e4e81b4ce52d9a2561f64c
ZeroUno <zerouno@slacky.it>
2012-10-17 05:02:46 +0200

    Repository slackware:         postgresql         geoclue         libmusicbrainz2         libmusicbrainz3         onig         portaudio         pyxdg         espeak         gespeaker         mbrola         sylpheed         kbackup         komparator4

---
+++ slackware/database/postgresql/doinst.sh
@@ -0,0 +1,30 @@
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/rc.d/rc.postgresql.new
+
+
+
+###
+# Create postgres user:group if they don't exist.
+###
+group_exists=`grep ^postgres etc/group`
+if [[ "${group_exists}" == "" ]]; then
+	groupadd -g 209 postgres
+fi
+user_exists=`grep ^postgres etc/passwd`
+if [[ "${user_exists}" == "" ]]; then
+	useradd -c "PostgreSQL ORDBMS" -d /var/lib/pgsql -u 209 -g 209 -s "/bin/false" postgres
+fi
+
+
+
