656b53ddde8105b7f0fde212a414e2db29961341
ZeroUno <zerouno@slacky.it>
2012-11-27 05:06:09 +0100

    Repository slackware:         dustrac         gnome-python-keyring         grilo-plugins         grilo         gssdp         gupnp-av         gupnp-igd         gupnp         librest         rhythmbox         xfburn

---
+++ slackware/multimedia/xfburn/doinst.sh
@@ -0,0 +1,13 @@
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database -q usr/share/applications/kde
+fi
+
+if [ -r usr/share/icons/hicolor/icon-theme.cache ]; then
+  usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database /usr/share/mime &> /dev/null
+fi
+
+
