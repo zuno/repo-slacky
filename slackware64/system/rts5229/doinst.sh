c8713a9ac1530d95426aec4273d5b50d2c3b0ac7
ZeroUno <zerouno@slacky.it>
2012-12-23 05:06:54 +0100

    Repository slackware64:         rts5229

--- slackware64/system/rts5229/doinst.sh
+++ slackware64/system/rts5229/doinst.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 depmod -a
+
+echo
+echo "Remember to add this line in the Realtek section"
+echo 'DRIVERS=="rts5229", ENV{ID_DRIVE_FLASH_SD}="1"'
+echo "in the following files:"
+echo "/lib/udev/rules.d/80-udisks.rules"
+echo "/lib/udev/rules.d/80-udisks2.rules"
+echo "to manage the mount of the SD/MMC devices"
+echo 
