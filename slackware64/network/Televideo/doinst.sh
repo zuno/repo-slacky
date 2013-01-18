50d3849e0f4b8b54f370c4e45182a62285ab5d53
ZeroUno <zerouno@slacky.it>
2013-01-18 05:07:32 +0100

    Repository slackware64:         physfs         Televideo

---
+++ slackware64/network/Televideo/doinst.sh
@@ -0,0 +1,9 @@
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database &> /dev/null
+fi
+
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+  /usr/bin/gtk-update-icon-cache  -f -q /usr/share/icons/hicolor &> /dev/null
+fi
+
