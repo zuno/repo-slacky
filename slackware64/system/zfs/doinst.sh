799eca7dcd536dc86e5c6edb69a7bf4e180caa56
ZeroUno <zerouno@slacky.it>
2013-04-22 05:09:44 +0200

    Repository slackware64:         zfs

--- slackware64/system/zfs/doinst.sh
+++ slackware64/system/zfs/doinst.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 depmod -a
+
+cat << EOF >> /etc/rc.d/rc.local
+
+# Start zfs service
+# If you do not wish this to be executed here then comment it out,
+# and the init system will skip it next time.
+if [ -x /etc/rc.d/rc.zfs ]; then
+    /etc/rc.d/rc.zfs start
+fi
+
+EOF
