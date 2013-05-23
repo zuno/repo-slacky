8ccb4a745020226401cc56345ed82fcc6c3f558b
ZeroUno <zerouno@slacky.it>
2013-05-23 05:16:38 +0200

    Repository slackware:         smplayer         skype

--- slackware/multimedia/smplayer/doinst.sh
+++ slackware/multimedia/smplayer/doinst.sh
@@ -1,28 +1,11 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-config etc/smplayer/input.conf.new
-
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database -q usr/share/applications/kde
+if [ -x /usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
-
-if [ -r usr/share/icons/hicolor/icon-theme.cache ]; then
-  usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor
-fi
-
 if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database /usr/share/mime &> /dev/null
+  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
-
-
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
